7851e86161f69ec78ce8cf793c67780e891ddfde
liveforget <forgetlive@outlook.com>
2019-09-02 19:43:21 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import url_for
 app = Flask(__name__)
 
+name = 'Grep Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Prefect World', 'year':'1993'},
+    {'title': 'Leon', 'year':'1994'},
+    {'title': 'Mahjon', 'year':'1996'},
+    {'title':'Swallowtail Butterfly', 'year':'1996'},
+    {'title':'King of Comedy', 'year':'1999'},
+    {'title':'Devils on the Doorstep', 'year':'1999'},
+    {'title':'WALL-E', 'year':'2008'},
+    {'title':'The Pork of Music', 'year':'2012'},
+]
+
+
+
+
+
+
+
 @app.route('/')
-def hello():
+def index():
     #return u'Welcome to My Watchlist!'
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    #return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
+
 
 @app.route('/home')
 def home():
